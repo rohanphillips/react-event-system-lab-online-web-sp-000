1fe589f7957b720fcd4cea0c23c4fa51fd367dab
rohanphillips <rohan@solutions-insurance.com>
2020-11-13 14:52:06 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+  
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     c\
+  //   }
+  // }
+
+  focus = () => {
+    console.log("Good!")
+  }
+
+  blur = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render() {
+    return (
+      <button onFocus={this.focus} onBlur={this.blur}>Test Me</button>
+    )
+  }
+  
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,32 @@
 // Code Keypad Component Here
+// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+  
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     c\
+  //   }
+  // }
+  handleSubmit = () => {
+
+  }
+
+  keyUp = (event) => {
+    console.log("Entering password...")
+  }
+  
+  render() {
+    return (
+      <form >
+        <label>
+          Password:
+          <input onKeyUp={this.keyUp} type="password" />
+        </label>
+      </form>
+    )
+  }
+  
+}
